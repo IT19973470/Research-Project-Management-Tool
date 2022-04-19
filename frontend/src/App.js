b8310bbddb911da5d0bf40aac4d8a832da2c858f
imalka <imalka@gmail.com>
2022-04-20 01:37:03 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentRegister from "./student/StudentRegister";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./login/Login";
+
+function App() {
+    return (
+      <Login/>
+    );
+}
+
+export default App;
---
+++ frontend/src/login/Login.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="row">
+            <div className="col-12" style={{fontSize:"50px"}}>
+                Research Project Management Tool
+            </div>
+            <div className="col-6">
+                456
+            </div>
+        </div>
+    )
+}
+
+export default Login;
