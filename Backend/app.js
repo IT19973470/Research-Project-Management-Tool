c4ac4910f82819a8873d91829e9b66fae7769db5
SashiniUdyani <sashiniudyani14@gmail.com>
2022-05-28 00:44:46 +0530

    View Topics

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/rpmt/user', require('./routers/user_routes'));
 app.use('/rpmt/student', require('./routers/student_routes'));
 app.use('/rpmt/admin', require('./routers/admin_routes'));
+app.use('/rpmt/panel_member', require('./routers/panel_member_routes'));
 
 app.listen(9000, () => {
     console.log('Server started')
